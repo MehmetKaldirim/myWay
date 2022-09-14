0a32581b7e3e7f3ccaa1e05162fe39538d877e48
Mehmet <mathdenizi@gmail.com>
2022-09-15 01:00:21 +0200

    ProgramServiceImpl  added

--- src/main/java/com/zerotohero/repository/ProgramRepository.java
+++ src/main/java/com/zerotohero/repository/ProgramRepository.java
@@ -1,8 +1,11 @@
 package com.zerotohero.repository;
 
 import com.zerotohero.entity.Program;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProgramRepository {
+public interface ProgramRepository extends JpaRepository<Program, Long> {
     Program findByProgramCode(String code);
+
+    Program findByUser_UserEmail(String userEmail);
 
 }
