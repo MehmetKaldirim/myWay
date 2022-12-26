9ad002fde895caac0d4163cc250cc117cf9da20b
Mehmet <mathdenizi@gmail.com>
2022-12-26 11:26:38 +0100

    Cross Origin

--- src/main/java/com/zerotohero/controller/UserControllerWithWrapper.java
+++ src/main/java/com/zerotohero/controller/UserControllerWithWrapper.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users/api/v2")
 public class UserControllerWithWrapper {
 
